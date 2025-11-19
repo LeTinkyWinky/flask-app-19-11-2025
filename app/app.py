74406ecc45cccaab1af5d6a7d3b674445c30df56
Tinky Winky <104074343+LeTinkyWinky@users.noreply.github.com>
2025-11-19 12:15:07 +0100

    to fix app.py work

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, render_template, request, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 
 # In-memory database
 items = []
--- tests/test_integration_example.py
+++ tests/test_integration_example.py
@@ -2,8 +2,8 @@ from runpy import run_path
 
 
 def test_integration_example():
-    module = run_path("app.py")
+    module = run_path("app/app.py")
     app = module['app']
     client = app.test_client()
     response = client.get('/')
-    assert response.status_code == 0
+    assert response.status_code == 200
